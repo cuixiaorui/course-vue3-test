222cc9f2a85ff6b544e005f9dfe7b276cce9c6b4
cuixiaorui <cui_xiaorui@126.com>
2020-11-15 14:06:50 +0800

    add snapshot test demo

---
+++ chapters/eight/test-snapshot/src/components/Date.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div>
+      {{ date }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    // const date = Date.now();
+    const date = new Date().getTime();
+    return {
+      date
+    };
+  }
+};
+</script>
+
+<style></style>
---
+++ chapters/eight/test-snapshot/src/components/Dynamic.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div v-if="!showB">A</div>
+    <div v-else>B</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["showB"]
+};
+</script>
+
+<style></style>
