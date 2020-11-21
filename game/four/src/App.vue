1e96a25239b67aab58c4df5ff50d4a0c2692d234
cuixiaorui <cui_xiaorui@126.com>
2020-11-21 19:11:00 +0800

    add game four

---
+++ game/four/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <TodoList></TodoList>
+</template>
+
+<script>
+import TodoList from "./components/TodoList";
+
+export default {
+  name: "App",
+  components: {
+    TodoList,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ game/four/src/components/__tests__/TodoList.spec.js
@@ -0,0 +1,10 @@
+import { shallowMount } from "@vue/test-utils";
+import TodoList from "../TodoList.vue";
+
+describe("TodoList.vue", () => {
+  it.todo("renders a todo", () => {});
+
+  it.todo("creates a todo", () => {});
+
+  it.todo("completes a todo", () => {});
+});
