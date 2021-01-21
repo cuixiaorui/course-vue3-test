79dd8a54c131fa00153434665fb2fd2d7767b304
cuixiaorui <cui_xiaorui@126.com>
2021-01-21 21:44:15 +0800

    feat: add 10-timer

---
+++ 10-timer/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <timer></timer>
+  </div>
+</template>
+
+<script>
+import Timer from "./components/Timer.vue";
+export default {
+  name: "App",
+  components: {
+    Timer
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
