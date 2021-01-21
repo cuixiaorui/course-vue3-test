19ffe48c2b88d0e4e2aceab40666640ee51f21b5
cuixiaorui <cui_xiaorui@126.com>
2021-01-21 21:44:59 +0800

    feat: add 12-vue-router

---
+++ 12-vue-router/src/components/Component.vue
@@ -0,0 +1,17 @@
+<template>
+  <button @click="redirect">Click to Edit</button>
+</template>
+<script>
+export default {
+  props: ["authenticated"],
+  methods: {
+    redirect() {
+      if (this.authenticated) {
+        this.$router.push(`/posts/${this.$route.params.id}/edit`);
+      } else {
+        this.$router.push("/404");
+      }
+    }
+  }
+};
+</script>
